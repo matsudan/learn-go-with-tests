feb4b6c3b5cda05b8fdd3e04cd7f0dadd8ea9150
matsudan <daaamatsun@gmail.com>
2020-07-06 00:44:44 +0900

    Use select and refactoring

--- select_statement/racer_test.go
+++ select_statement/racer_test.go
@@ -24,7 +24,7 @@ func TestRacer(t *testing.T) {
 		wantWinner string
 	}{
 		{
-			name: "with two args",
+			name: "with slow & fast urls",
 			args: args{
 				slowURL: slowServer.URL,
 				fastURL: fastServer.URL,
@@ -46,4 +46,82 @@ func makeDelayedServer(delay time.Duration) *httptest.Server {
 		time.Sleep(delay)
 		w.WriteHeader(http.StatusOK)
 	}))
+}
+
+func TestRacerWithSelect(t *testing.T) {
+	serverA := makeDelayedServer(20 * time.Millisecond)
+	serverB := makeDelayedServer(0 * time.Millisecond)
+
+	defer serverA.Close()
+	defer serverB.Close()
+
+	type args struct {
+		slowURL string
+		fastURL string
+		timeout time.Duration
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantWinner string
+	}{
+		{
+			name: "",
+			args: args{
+				slowURL: serverA.URL,
+				fastURL: serverB.URL,
+				timeout: 10 * time.Second,
+			},
+			wantWinner: serverB.URL,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotWinner, err := RacerWithSelect(tt.args.slowURL, tt.args.fastURL)
+
+			if err != nil {
+				t.Fatalf("did not expect an error but got one %v", err)
+			}
+
+			if gotWinner != tt.wantWinner {
+				t.Errorf("got %q, want %q", gotWinner, tt.wantWinner)
+			}
+		})
+	}
+}
+
+func TestConfigurableRacer(t *testing.T) {
+	server := makeDelayedServer(25 * time.Millisecond)
+
+	defer server.Close()
+
+	type args struct {
+		aURL    string
+		bURL    string
+		timeout time.Duration
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantWinner string
+		wantErr    bool
+	}{
+		{
+			name: "test configurable racer",
+			args: args{
+				aURL:    server.URL,
+				bURL:    server.URL,
+				timeout: 20 * time.Millisecond,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := ConfigurableRacer(tt.args.aURL, tt.args.bURL, tt.args.timeout)
+
+			if err == nil {
+				t.Error("expected an error but didn't get one")
+			}
+		})
+	}
 }
